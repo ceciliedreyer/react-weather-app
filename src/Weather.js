7c2654ac4d84667eb035985365e0aea28cf1c45c
ceciliedreyer <149535863+ceciliedreyer@users.noreply.github.com>
2024-04-26 09:42:44 +0200

    Updated React and css so basic weather app showing city and current temperature

--- src/Weather.js
+++ src/Weather.js
@@ -4,38 +4,77 @@ import axios from "axios";
 import "./Weather.css";
 
 export default function Weather() {
-  const [city, setCity] = useState("Skæring");
+  const [city, setCity] = useState("Aarhus");
   const [weather, setWeather] = useState(null);
+  const [loaded, setLoaded] = useState(false);
 
   function updateWeather(response) {
-    setWeather(response.data.main.temp);
+    setWeather({
+      temperature: response.data.main.temp,
+      town: response.data.name,
+    });
   }
 
   function updateCity(event) {
+    setLoaded(true);
     setCity(event.target.value);
   }
 
   function handleSearch(event) {
     event.preventDefault();
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=ac209dae1f283fb332a5bb7f50b0f468&units=metric`;
+    let apiKey = "9dc3b95c42fa941f8763112d84a09d1e";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(updateWeather);
   }
 
-  return (
-    <div className="Weather">
-      <form className="search-bar" onClick={handleSearch}>
-        <input
-          type="search"
-          placeholder="Search for a city..."
-          onChange={updateCity}
-        />
-        <input type="submit" value="search" />
-      </form>
-      <div className="results">
-        <h2>
-          temp is {weather}ºC in {city}
-        </h2>
+  if (loaded) {
+    return (
+      <div>
+        <div className="header-form">
+          <form class="search-form" onClick={handleSearch}>
+            <input
+              type="search"
+              placeholder="Search for a city..."
+              onChange={updateCity}
+              className="search-form-input"
+            />
+            <input
+              type="submit"
+              value="search"
+              className="search-form-button"
+            />
+          </form>
+        </div>
+        <div className="Weather">
+          <div className="header-info">
+            <div className="current-location">{weather.town}</div>
+          </div>
+          <div className="results">
+            <h2>{Math.round(weather.temperature)}</h2>
+            <p className="degree-icon">ºC</p>
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div>
+        <div className="header-form">
+          <form class="search-form" onClick={handleSearch}>
+            <input
+              type="search"
+              placeholder="Search for a city..."
+              onChange={updateCity}
+              className="search-form-input"
+            />
+            <input
+              type="submit"
+              value="search"
+              className="search-form-button"
+            />
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
