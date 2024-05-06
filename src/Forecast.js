40a7aa5c34793cdc47f70c7d589ba20a98507708
ceciliedreyer <149535863+ceciliedreyer@users.noreply.github.com>
2024-05-06 10:30:22 +0200

    Added Loaded state

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,39 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import "./Forecast.css";
 
 export default function Forecast(props) {
-  console.log(props.coord);
+  const [loaded, setLoaded] = useState(false);
 
   function getForecast(response) {
+    setLoaded(true);
     console.log(response.data);
   }
 
-  let lat = props.coord.lat;
-  let lon = props.coord.lon;
-
-  let apiKey = "9dc3b95c42fa941f8763112d84a09d1e";
-
-  let apiURL = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-
-  axios.get(apiURL).then(getForecast);
-
-  return (
-    <div className="Forecast">
-      <div className="row">
-        <div className="col">
-          <div className="forecast-day">Fri</div>{" "}
-          <div>
-            <img
-              className="forecast-icon"
-              src="https://openweathermap.org/img/wn/10d@2x.png"
-              alt="weather-icon"
-            ></img>
-          </div>{" "}
-          <span className="forecast-max">21º</span>{" "}
-          <span className="forecast-min">11º</span>
+  if (loaded) {
+    return (
+      <div className="Forecast">
+        <div className="row">
+          <div className="col">
+            <div className="forecast-day">Fri</div>{" "}
+            <div>
+              <img
+                className="forecast-icon"
+                src="https://openweathermap.org/img/wn/10d@2x.png"
+                alt="weather-icon"
+              ></img>
+            </div>{" "}
+            <span className="forecast-max">21º</span>{" "}
+            <span className="forecast-min">11º</span>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let lat = props.coord.lat;
+    let lon = props.coord.lon;
+
+    let apiKey = "ce144f0cf51fa43f03431f0488a36728";
+
+    let apiURL = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+
+    axios.get(apiURL).then(getForecast);
+  }
 }
